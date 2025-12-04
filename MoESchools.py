e8747905f8514edd472603b109373a1c87626c20
Rhys Edlin <104714623+Polrek@users.noreply.github.com>
2025-12-04 14:27:02 +1300

    tidy comment

--- MoESchools.py
+++ MoESchools.py
@@ -16,7 +16,6 @@ from tkinter import ttk, filedialog, messagebox, scrolledtext # for UI widgets
 import requests # for HTTP requests
 import pandas as pd # for data manipulation and Excel export
 
-
 # --------------------------- Proxy Handler ----------------------------
 
 
